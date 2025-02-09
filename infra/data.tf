7f39287bdc62f17dc80824ca6e717b813bc2675a
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-09 09:22:55 -0300

    Permissao API Gateway para Lambda

--- infra/data.tf
+++ infra/data.tf
@@ -3,7 +3,3 @@ data "archive_file" "lambda" {
   source_dir  = "../${path.module}/src"
   output_path = "../${path.module}/lambda.zip"
 }
-
-data "aws_api_gateway_rest_api" "apigateway_rest_api" {
-  name = "Pos Tech Hackathon - API Gateway"
-}
